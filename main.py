d491c49d047e2559cc8c1ba0f868613e949d9ad0
Moises Rodriguez <113930437+Msrodriguez17@users.noreply.github.com>
2022-10-04 17:36:42 +0000

    Endless loops

---
+++ main.py
@@ -0,0 +1,37 @@
+# fork the repl.it and create a new git hub repo entitled
+#Comparison operators
+# Logical operators
+# Decision making
+# loops(for loops, while loops, range, enumerator)
+# min/max practice
+# Random in python
+# List comprehension
+#importing modules
+#modularized your code
+#work on creating and understanding functions
+
+# from decision_making import decisions
+# from comparison_operators import comparison
+# from dictionaries_loops import dictionaries_loops
+from loops import loop
+# from enumerators import enumerator
+# from list_comprehension import list_comprehensions
+# from logical_operators import logical_operator
+# from min_max import min_maximum
+# from random import randoms
+# from ranges import range
+# from while_loops import while_loop
+# from zip import zips
+
+
+# decisions()
+# comparison()
+loop()
+# enumerator()
+# list_comprehensions()
+# logical_operator()
+# min_maximum()
+# randoms()
+# range()
+# while_loop()
+# zips()
